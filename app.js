a1e2b0966c212d7f8ecbac41dfe81ad7be475760
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-25 23:49:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     const walletInfo = document.getElementById('walletInfo');
     const walletAddressSpan = document.getElementById('walletAddress');
 
-    // Wallet Connection State
+    // Wallet Connection Sta
     let walletProvider = null;
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
