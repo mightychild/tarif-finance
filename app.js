2ce7cc36983e8f0d6cab21279d3d118e19f18561
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-28 21:34:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async function() {
-    // DOM Elements
+    // DOM 
     const connectWalletBtn = document.getElementById('connectWalletBtn');
     const walletModal = document.getElementById('walletModal');
     const closeModal = document.getElementById('closeModal');
